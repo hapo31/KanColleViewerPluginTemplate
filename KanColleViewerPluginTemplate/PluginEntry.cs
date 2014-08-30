2389b64dfdd04062b35c859a89d6e1229438dffa
happou31 <happo31@gmail.com>
2014-08-31 02:18:06 +0900

    ファイルと参照の追加
    PluginEntry.cs KanColleViewer側から呼ばれるモジュール
    参照設定にSystem.ComponentModel.Compositionを追加

---
+++ KanColleViewerPluginTemplate/PluginEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+
+using Grabacr07.KanColleViewer.Composition;
+using Grabacr07.KanColleWrapper;
+
+namespace KanColleViewerPluginTemplate
+{
+	[Export(typeof(IToolPlugin))]
+	[ExportMetadata("Title", "")]
+	[ExportMetadata("Description", "")]
+	[ExportMetadata("Version", "")]
+	[ExportMetadata("Author", "")]
+	public class PluginEntry : IToolPlugin
+	{
+
+		public string ToolName
+		{
+			get { return "Plugin Name"; }
+		}
+
+		public object GetToolView()
+		{
+			return null;
+		}
+
+		public object GetSettingsView()
+		{
+			return null;
+		}
+	}
+}
